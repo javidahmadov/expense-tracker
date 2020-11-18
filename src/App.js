0118269e99eb1941a8f067573c9a66033c036b3c
javidahmadov <cavidahmadov111@gmail.com>
2020-11-18 10:29:38 +0400

    First

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import AddTransactions from './components/AddTransactions';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransactions />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
